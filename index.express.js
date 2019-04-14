1bb5ab1b345419c15c71d76f55fd6c7b0e7755ee
ruanyf <yifeng.ruan@gmail.com>
2019-04-14 22:51:43 +0800

    feat: use koa

--- index.express.js
+++ index.express.js
@@ -1,63 +1,20 @@
 // Import the express lirbary
-// const express = require('express')
+const express = require('express')
+
+// Import the axios library, to make HTTP requests
+const axios = require('axios')
 
 // This is the client ID and client secret that you obtained
 // while registering the application
 const clientID = '7e015d8ce32370079895'
 const clientSecret = '2b976af0e6b6ceea2b1554aa31d1fe94ea692cd9'
 
-const Koa = require('koa');
-const path = require('path');
-const serve = require('koa-static');
-const route = require('koa-route');
-const axios = require('axios');
-
-const app = new Koa();
-
-// Import the axios library, to make HTTP requests
-
-
-
 // Create a new express application and use
 // the express static middleware, to serve all files
 // inside the public directory
-const main = serve(path.join(__dirname + '/public'));
-app.use(main);
+const app = express()
+app.use(express.static(__dirname + '/public'))
 
-const oauth = async ctx => {
-  const requestToken = ctx.request.query.code;
-  console.log('authorization code:', requestToken);
-  const tokenResponse = await axios({
-    // make a POST request
-    method: 'post',
-    // to the Github authentication API, with the client ID, client secret
-    // and request token
-    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-    // Set the content type header, so that we get the response in JSOn
-    headers: {
-      accept: 'application/json'
-    }
-  });
-  const accessToken = tokenResponse.data.access_token;
-  console.log(`access token: ${accessToken}`);
-
-  const result = await axios({
-    method: 'get',
-    url: `https://api.github.com/user`,
-    headers: {
-      accept: 'application/json',
-      Authorization: `token ${accessToken}`
-    }
-  });
-  console.log(result.data);
-  const name = result.data.name;
-
-  ctx.response.redirect(`/welcome.html?name=${name}`);
-};
-
-app.use(route.get('/oauth/redirect', oauth));
-
-/*
 app.get('/oauth/redirect', (req, res) => {
   // The req.query object has the query params that
   // were sent to this route. We want the `code` param
@@ -77,7 +34,7 @@ app.get('/oauth/redirect', (req, res) => {
     // Once we get the response, extract the access token from
     // the response body
     const accessToken = response.data.access_token;
-
+    /*
     return axios({
       method: 'get',
       url: `https://api.github.com/user`,
@@ -86,14 +43,14 @@ app.get('/oauth/redirect', (req, res) => {
         Authorization: `token ${accessToken}`
       }
     });
-
+    */
     // redirect the user to the welcome page, along with the access token
-    // res.redirect(`/welcome.html?access_token=${accessToken}`)
+    res.redirect(`/welcome.html?access_token=${accessToken}`)
   }).then(response => {
     // console.log(response.data);
     // res.redirect(`/welcome.html`)
   })
 })
-*/
+
 // Start the server on port 8080
-app.listen(8080);
+app.listen(8080)
